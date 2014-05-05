ee685dcaa351a9340fd4bc4a87bb992948845c5f
Rescue Themes <hello@rescuethemes.com>
2014-05-05 09:20:02 -0500

    Version 1.2.1
    Updated links and added readme.txt

--- rescue-shortcodes.php
+++ rescue-shortcodes.php
@@ -5,7 +5,7 @@
  Description: A lightweight shortcodes plugin for themes developed by <a href="http://themeforest.net/user/RescueThemes">Rescue Themes</a>
  Author: Rescue Themes
  Author URI: http://themeforest.net/user/RescueThemes
- Version: 1.2
+ Version: 1.2.1
  License: GNU General Public License version 2.0
  License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
